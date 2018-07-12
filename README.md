489243a8dfe0e0e0d7c35f5b578a1ba9f08bd418
samarth sikotara <samarthsikotara@gmail.com>
2018-07-13 00:35:20 +0530

    Added nodejs scrapper files

---
+++ README.md
@@ -0,0 +1,37 @@
+## NodeJs Scrapper v1.0
+
+## Installation
+####1. Clone this project or Download that ZIP file
+
+####2.  Make sure you have [npm](https://www.npmjs.org/) installed globally
+ 
+ 
+```sh
+$ sudo apt-get install npm
+```
+####3. On the command prompt run the following commands
+
+```sh
+$ cd `project-directory`
+```
+- Clean your npm cache
+
+```sh
+$ npm cache clean --force
+```
+
+- To install dependency npm packages for app
+
+```sh
+$ npm install
+```
+
+- To run the scrapper
+```sh
+$ node scrapper.js
+```
+
+- Urls will be in this below file
+```sh
+$ text.txt
+```
---
+++ scrapper.js
@@ -0,0 +1,53 @@
+var request = require('request');
+var Promise = require('bluebird');
+var getPromisify = Promise.promisify(request.get);
+var htmlparser = require("htmlparser2");
+var fs = require('fs');
+
+var maxAllowedConnections = 5;
+
+module.exports = {
+
+	fetchData : function(url){
+		return getPromisify(url).then((url_res) => {
+			var all_urls = []
+			var body = url_res.body;
+			console.log(url)
+			console.log('========')
+			var parser = new htmlparser.Parser({
+				onopentag: function(name, attribs){
+					if(name === "a" && attribs.href){
+						if(attribs.href.indexOf('medium.com') >= 0){
+							all_urls.push(attribs.href)
+							fs.appendFile('test.txt', attribs.href+'\n', function(err) {
+								if(err) {
+						      return console.log(err);
+						    }
+						  })
+							console.log("Url!========> "+attribs.href);
+						}
+					}
+				}
+			}, {decodeEntities: true});
+			parser.write(body);
+			parser.end()
+			console.log(all_urls)
+
+			//awaits for returned promises
+			return Promise.map(all_urls, module.exports.fetchData, { concurrency: maxAllowedConnections })
+      .then((final_results) => {
+      	var data = {};
+        for (var i = 0; i < final_results.length; i++)
+           data[all_urls[i]] = final_results[i];
+        
+        return data;
+      });
+		})
+
+	}
+}
+
+module.exports.fetchData('https://medium.com').then((success) => {
+	console.log(success)
+	process.exit(1);
+})
