1f8377b279d8b328201cb3c0ef383eb49760ed96
samarth sikotara <samarthsikotara@gmail.com>
2018-07-14 00:12:01 +0530

    Fixed duplicate urls

--- scrapper.js
+++ scrapper.js
@@ -3,6 +3,9 @@ var Promise = require('bluebird');
 var getPromisify = Promise.promisify(request.get);
 var htmlparser = require("htmlparser2");
 var fs = require('fs');
+var _ = require('lodash')
+var csv = require("fast-csv");
+var readline = require('readline');
 
 var maxAllowedConnections = 5;
 
@@ -19,20 +22,26 @@ module.exports = {
           if(name === "a" && attribs.href){
             if(attribs.href.indexOf('medium.com') >= 0){
               all_urls.push(attribs.href)
-              fs.appendFile('test.txt', attribs.href+'\n', function(err) {
-                if(err) {
-                  return console.log(err);
-                }
-              })
-              console.log("Url!========> "+attribs.href);
             }
           }
         }
       }, {decodeEntities: true});
       parser.write(body);
       parser.end()
+      var all_urls = _.uniq(all_urls);
       console.log(all_urls)
-
+      all_urls.forEach(function(url){
+        module.exports.readFile('test.csv', url).then((file_url_present) => {
+          if(file_url_present){
+          }else{
+            fs.appendFile('test.csv', url+'\n', function(err) {
+              if(err) {
+                return console.log(err);
+              }
+            })
+          }
+        })
+      })
       //awaits for returned promises
       return Promise.map(all_urls, module.exports.fetchData, { concurrency: maxAllowedConnections })
       .then((final_results) => {
@@ -44,8 +53,31 @@ module.exports = {
       });
     })
 
+  },
+
+  readFile : function (file, url) {
+    return new Promise(function (resolve, reject) {
+        var lines = [];
+        var rl    = readline.createInterface({
+            input: fs.createReadStream(file)
+        });
+
+        rl.on('line', function (line) {
+          lines.push(line)
+        });
+
+        rl.on('close', function () {
+          if(lines.indexOf(url) > 0){
+            resolve(true)
+          }else{
+            resolve(false)
+          }
+        });
+    });
   }
+
 }
+
 
 module.exports.fetchData('https://medium.com').then((success) => {
   console.log(success)
